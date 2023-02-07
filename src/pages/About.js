a5510b9c17c0cf916185c6120d63c1e0ddd9b07b
Nick Zabkar <nzabkar51@gmail.com>
2023-02-07 03:12:37 -0500

    Added styling to card class

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
+  <section className='card'>
+      <header className='card-header'>
+        <h1>About Me</h1>
+      </header>
+      <article className='card-body'>
+        <img alt='profile' src='https://github.com/CIDoubleAgent/nz-portfolio-v3/blob/main/src/images/profile/profilePicMe_1.jpg?raw=true'></img>
+      </article>
+    </section>
 }
 
 export default About
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,14 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='card'>
-      <header>
+    <section className='card'>
+      <header className='card-header'>
         <h1>WELCOME TO MY PROFESSIONAL PORTFOLIO</h1>
       </header>
-    </div>
+      <article className='card-body'>
+        {/* <img src='https://github.com/CIDoubleAgent/nz-portfolio-v3/blob/main/src/images/profile/profilePicMe_1.jpg?raw=true'></img> */}
+      </article>
+    </section>
   )
 }
 
