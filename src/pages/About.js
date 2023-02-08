863343f5d95b57105ead3b287123d55d18d39796
Nick Zabkar <nzabkar51@gmail.com>
2023-02-08 00:20:25 -0500

    Added to about section styling

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,6 +8,9 @@ const About = () => {
       </header>
       <article className='card-body'>
         <img alt='profile' src='https://github.com/CIDoubleAgent/nz-portfolio-v3/blob/main/src/images/profile/profilePicMe_1.jpg?raw=true'></img>
+        <p>
+          About me text goes here asdfa sdf adf asdf asfas dfsdf asf jklhk lhjk sdf adsf asdf asdf dsfg fgdh hgj jhk ghjkl hgjkll kjh hjk ghjk hgjk ghjk hjkg hjgk ghjk hgjk hjk hjk ghjk ghjk jhgk hjkg hgjk ghjk hgjk ghjk ghjk ghjk ghjk ghjk ghjk gh jkgh kghk jgh jkghjk gh jkgh
+        </p>
       </article>
     </section>
   )
