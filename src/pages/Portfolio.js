2dbb17c6c886442868d7326a29405d9fc3f3d5a9
Nick Zabkar <nzabkar51@gmail.com>
2023-02-19 19:18:07 -0500

    Added structure to portfolio section

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <div className='portfolio-wrapper'>
+      <h1>Portfolio</h1>
+      <div className='portfolio-grid'>
+
+      </div>
+    </div>
   )
 }
 
