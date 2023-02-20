895e79fd92e12d4050f248d503366bcbefbf334b
Nick Zabkar <nzabkar51@gmail.com>
2023-02-19 19:17:08 -0500

    Added about card image

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,14 +8,10 @@ const About = () => {
 
       <div className='card about-card'>
 
-        <article className='card-body'>
-          {/* <img alt='profile' src='https://github.com/CIDoubleAgent/nz-portfolio-v3/blob/main/src/images/profile/profilePicMe_1.jpg?raw=true'>
-          </img> */}
+        <img alt='profile' src='https://github.com/CIDoubleAgent/nz-portfolio-v3/blob/main/src/images/profile/profilePicMe_1.jpg?raw=true'>
+        </img>
 
-          <h1 className='card-header'>
-            About Me
-          </h1>
-        
+        <article className='card-body'>
           <p>
             I am a web developer from Dover New Hampshire and have been a carpenter by trade for most of my career. My hobbies include hiking the many beautiful trails and mountains of New Hampshire, playing basketball, wakeboarding, enjoying craft beer, and poking holes in movie plots.
           </p>
