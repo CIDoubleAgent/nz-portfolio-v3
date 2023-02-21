edf6c318997969400e9ef2332776366b8d48d1ee
Nick Zabkar <nzabkar51@gmail.com>
2023-02-21 07:38:31 -0500

    Repositioned logo

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -3,13 +3,14 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className='hero-image' alt='modo-2-keyboard'>
-      <div className='logo'>
-        <p>Ž</p>
-      </div>
 
       <div className='hero-text'>
         <h1>Hero section</h1>
         <p>Hero text</p>
+      </div>
+      
+      <div className='logo'>
+        <p>Ž</p>
       </div>
     </div>
   )
