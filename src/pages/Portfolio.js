ce1b0a468012f8e59d906d316ab942832b18d8ff
Nick Zabkar <nzabkar51@gmail.com>
2023-02-28 01:15:01 -0500

    Added portfolio section

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,18 +1,65 @@
-import React from 'react'
+import React, { Component } from 'react';
+import ProjectData from '../data/projects.json';
+// import image from '../images/project-images/hungry-snake_1.png'
 
-const Portfolio = () => {
-  return (
-    <section>
-      <div className='portfolio-image background-image'></div>
+class Portfolio extends Component {
+  render() {
+    return (
+      <section>
+        <div className='portfolio-image background-image'></div>
 
-      <div className='portfolio'>
-        <h1>Portfolio</h1>
-        <div className='portfolio-grid'>
+        <div className=' portfolio'>
+          {ProjectData.map((projectDetail) => {
+            return <article className='card' key={projectDetail.id}>
 
+              <h2>
+                {projectDetail.title}
+              </h2>
+
+              <div className='spacer'></div>
+
+              <p>
+                {projectDetail.description}
+              </p>
+
+              <ul>
+                <li>
+                  <a href={projectDetail.deployedLink}>
+                    Deployed Site
+                  </a>
+                </li>
+
+                <li>
+                  <a href={projectDetail.repoLink}>
+                    Repository
+                  </a>
+                </li>
+              </ul>
+            </article>
+          })}
         </div>
-      </div>
-    </section>
-  )
+      </section>
+    )
+
+
+  }
 }
+
+// export default ProjectList
+
+// const Portfolio = () => {
+//   return (
+//     <section>
+//       <div className='portfolio-image background-image'></div>
+
+//       <div className='portfolio'>
+//         <h1>Portfolio</h1>
+//         <div className='portfolio-grid'>
+
+//         </div>
+//       </div>
+//     </section>
+//   )
+// }
 
 export default Portfolio
