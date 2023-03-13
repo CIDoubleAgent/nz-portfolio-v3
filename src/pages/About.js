fd08d4c3b7d96152f63794407e53635fc75fcb84
Nick Zabkar <nzabkar51@gmail.com>
2023-03-13 01:36:03 -0400

    Added background image to about card

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,19 +2,20 @@ import React from 'react'
 
 const About = () => {
   return (
-    <section className='about-wrapper'>
+    <section>
+      <div className='about-image background-image' alt='coffee and croissants'></div>
 
-      <div className='about-card'>
+      <article className='about-card'>
 
         <img alt='profile' src='https://github.com/CIDoubleAgent/nz-portfolio-v3/blob/main/src/images/profile/profilePicMe_1.jpg?raw=true'>
         </img>
 
         <article className='card-body'>
           <p>
-            I am a web developer from Dover New Hampshire and have been a carpenter by trade for most of my career. My hobbies include hiking the many beautiful trails and mountains of New Hampshire, playing basketball, wakeboarding, enjoying craft beer, and poking holes in movie plots.
+            I am a web developer from Dover New Hampshire. My hobbies include hiking the many beautiful trails and mountains of New Hampshire, playing basketball, wakeboarding, enjoying craft beer, and poking holes in movie plots.
           </p>
         </article>
-      </div>
+      </article>
 
     </section>
   )
