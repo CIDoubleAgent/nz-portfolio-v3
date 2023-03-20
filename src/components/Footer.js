4d02d17fc869cc056b4cad31c8be8e805be73038
Nick Zabkar <nzabkar51@gmail.com>
2023-03-20 03:31:00 -0400

    Made footer link open in a new tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,16 @@
-import React from 'react'
-
 const Footer = () => {
   return (
     <div className='footer'>
       <ul>
 
         <li>
-          <a href='https://github.com/CIDoubleAgent'>
+          <a href='https://github.com/CIDoubleAgent' target='_blank' rel="noreferrer">
             Github
           </a>
         </li>
         
         <li>
-          <a href='https://www.linkedin.com/in/nicholas-zabkar-a730b457/'>
+          <a href='https://www.linkedin.com/in/nicholas-zabkar-a730b457/' target='_blank' rel="noreferrer">
             Linked in
           </a>
         </li>
