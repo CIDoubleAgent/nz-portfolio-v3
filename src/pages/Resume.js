8d2ffb9da79ce1292c0a91970e21f8df286e7943
Nick Zabkar <nickz51dev@gmail.com>
2023-07-20 05:13:50 -0400

    Added resume to resume page

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -5,11 +5,9 @@ const Resume = () => {
     <section>
       <div className='resume-image background-image'></div>
 
-      <div>Resume Goes Here</div>
-
-      {/* <div className='resume'>
-        <iframe title="resume" src="https://docs.google.com/document/d/1IHctUDXitDshLIJZ_pgVyyoPuoAp94CtnxQC489VYgs/edit"></iframe>
-      </div> */}
+      <div className='resume'>
+        <iframe title="resume" src="https://docs.google.com/document/d/1IHctUDXitDshLIJZ_pgVyyoPuoAp94CtnxQC489VYgs"></iframe>
+      </div>
     </section>
   )
 };
