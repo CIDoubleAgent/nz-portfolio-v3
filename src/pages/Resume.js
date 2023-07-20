96200308c330a46a13aee1ddaed01ceec13e31bd
Nick Zabkar <nzabkar51@gmail.com>
2023-07-19 20:26:50 -0400

    Added background image to resume section

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -3,10 +3,11 @@ const Resume = () => {
   
   return (
     <section>
-      {/* <div className='resume-image background-image' alt='modo-2-keyboard'></div> */}
+      <div className='resume-image background-image'></div>
 
-      <div className='resume'>
-      </div>
+      {/* <div className='resume'>
+        <iframe title="resume" src="https://docs.google.com/document/d/1IHctUDXitDshLIJZ_pgVyyoPuoAp94CtnxQC489VYgs/edit"></iframe>
+      </div> */}
     </section>
   )
 };
