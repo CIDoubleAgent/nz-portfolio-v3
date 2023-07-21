e3f513f630a8fae0b9f81ca818f53cc7594594b5
Nick Zabkar <nickz51dev@gmail.com>
2023-07-21 04:39:20 -0400

    Fixed resume display, added download button

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -2,12 +2,14 @@
 const Resume = () => {
   
   return (
-    <section>
+    <section className="resume-section">
       <div className='resume-image background-image'></div>
 
-      <div className='resume'>
-        <iframe title="resume" src="https://docs.google.com/document/d/1IHctUDXitDshLIJZ_pgVyyoPuoAp94CtnxQC489VYgs"></iframe>
-      </div>
+      <a className="download-button" href="path_to_file" download="Nick Zabkar Resume">Click Here To Download</a>
+
+      <article className='resume-pdf'>
+        <iframe title="Resume" src="https://docs.google.com/document/d/e/2PACX-1vTMLsviRDcxTnpF9JkDH_qzTNIgczOvFvntT7GkFuomLd60xSOj_QfT5sHmCPLHI6MRIssXvV3gbFMk/pub?embedded=true"></iframe>
+      </article>
     </section>
   )
 };
