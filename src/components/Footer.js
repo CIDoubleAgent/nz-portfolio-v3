5f968eec4efab0f79c3421469ca28ee6e61a9875
Nick Zabkar <nickz51dev@gmail.com>
2023-07-21 02:29:36 -0400

    Converted footer links to icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,16 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa"
+
 const Footer = () => {
   return (
     <div className='footer'>
       <ul>
 
         <li>
-          <a href='https://github.com/CIDoubleAgent' target='_blank' rel="noreferrer">
-            Github
-          </a>
+        <a href='https://github.com/CIDoubleAgent' target='_blank' rel="noreferrer"><FaGithub /></a>
         </li>
         
         <li>
-          <a href='https://www.linkedin.com/in/nicholas-zabkar-a730b457/' target='_blank' rel="noreferrer">
-            Linked in
-          </a>
+          <a href='https://www.linkedin.com/in/nicholas-zabkar-a730b457/' target='_blank' rel="noreferrer"><FaLinkedin /></a>
         </li>
 
       </ul>
