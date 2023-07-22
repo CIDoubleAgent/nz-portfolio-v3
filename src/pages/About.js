226cde391d17b9135e5aa7876d2c64c7a88d5338
Nick Zabkar <nickz51dev@gmail.com>
2023-07-22 01:57:12 -0400

    Refactored classes

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,16 +5,16 @@ const About = () => {
     <section>
       <div className='about-image background-image' alt='coffee and croissants'></div>
 
-      <article className='about-card'>
+      <article className='about-wrapper'>
 
 
-        <div className='card-body'>
+        <div className='about-card'>
 
           <img alt='profile' src='https://github.com/CIDoubleAgent/nz-portfolio-v3/blob/main/src/images/profile/profilePicMe_1.jpg?raw=true'>
           </img>
 
           <p>
-            I am a web developer from Dover New Hampshire. My hobbies include hiking the many beautiful trails and mountains of New Hampshire, playing basketball, wakeboarding, enjoying craft beer, and poking holes in movie plots.
+            I'm a web developer from Dover New Hampshire. My hobbies include hiking the many beautiful trails and mountains of New Hampshire, playing basketball, wakeboarding, enjoying craft beer, and poking holes in movie plots.
           </p>
         </div>
       </article>
