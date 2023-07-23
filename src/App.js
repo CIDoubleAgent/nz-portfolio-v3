c989cdd871aac77c8e8981fd87b7c896da45ffd1
Nick Zabkar <nickz51dev@gmail.com>
2023-07-23 17:56:27 -0400

    Run build

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Resume from "./pages/Resume";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="portfolio" element={<Portfolio />} />
+            <Route path="resume" element={<Resume />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,35 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Portfolio from "./pages/Portfolio";
-import Resume from "./pages/Resume";
-import Contact from "./pages/Contact";
-import NoPage from "./pages/NoPage";
+// import Layout from "./pages/Layout";
+// import Home from "./pages/Home";
+// import About from "./pages/About";
+// import Portfolio from "./pages/Portfolio";
+// import Resume from "./pages/Resume";
+// import Contact from "./pages/Contact";
+// import NoPage from "./pages/NoPage";
 
 import './scss/index.css';
 
 import reportWebVitals from './reportWebVitals';
 
-function App() {
-  return (
-    <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="portfolio" element={<Portfolio />} />
-            <Route path="resume" element={<Resume />} />
-            <Route path="contact" element={<Contact />} />
-            <Route path="*" element={<NoPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-  );
-}
+// function App() {
+//   return (
+//     <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<Layout />}>
+//             <Route index element={<Home />} />
+//             <Route path="about" element={<About />} />
+//             <Route path="portfolio" element={<Portfolio />} />
+//             <Route path="resume" element={<Resume />} />
+//             <Route path="contact" element={<Contact />} />
+//             <Route path="*" element={<NoPage />} />
+//           </Route>
+//         </Routes>
+//       </BrowserRouter>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
