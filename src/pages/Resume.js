38a4436e132153e0e059cab45262c9a5df7ebcd3
Nick Zabkar <nickz51dev@gmail.com>
2023-09-21 17:45:45 -0400

    Cleaned up

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -6,12 +6,13 @@ const Resume = () => {
     <section className="resume-section">
       <div className='resume-image background-image'></div>
 
-      {/* <a className="download-button" href="https://github.com/CIDoubleAgent/nz-portfolio-v3/blob/main/src/images/resume/Nicholas%20Zabkar%20Dev%20Resume%207_20_2023.pdf" target='_blank' rel="noreferrer" download="Nick Zabkar Resume">Click Here To Download</a> */}
-
       <DownloadButton />
 
       <article className='resume-pdf'>
-        <iframe title="Resume" src="https://docs.google.com/document/d/e/2PACX-1vTMLsviRDcxTnpF9JkDH_qzTNIgczOvFvntT7GkFuomLd60xSOj_QfT5sHmCPLHI6MRIssXvV3gbFMk/pub?embedded=true"></iframe>
+        <iframe 
+        title="Resume" 
+        src="https://docs.google.com/document/d/e/2PACX-1vTMLsviRDcxTnpF9JkDH_qzTNIgczOvFvntT7GkFuomLd60xSOj_QfT5sHmCPLHI6MRIssXvV3gbFMk/pub?embedded=true"
+        />
       </article>
     </section>
   )
